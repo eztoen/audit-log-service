180f5da604dbd8d7c3aa0b1d25037826bcabdc7d
eztoen <ikucheren0510@gmail.com>
2026-02-25 17:55:55 +0600

    - Create class for login (LoginSchema) - Add stricter data validation
    TODO:
    - Create password validation with field_validator

--- app/api/v1/auth/schemas.py
+++ app/api/v1/auth/schemas.py
@@ -1,8 +1,16 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field, EmailStr, field_validator
 
 class RegisterSchema(BaseModel):
-    username: str = Field()
-    name:     str = Field()
-    surname:  str = Field()
+    username: str = Field(..., min_length=5, max_length=30, pattern=r"^[a-zA-Z0-9_]")
+    name:     str = Field(..., min_length=2, max_length=50, pattern=r"\p{L}+")
+    surname:  str = Field(..., min_length=2, max_length=50, pattern=r"\p{L}+")
+    email:    EmailStr
+    password: str
+    
+    @field_validator('password')
+    def validate_password(cls, v):
+        ...
+    
+class LoginSchema(BaseModel):
     email:    EmailStr = Field()
     password: str = Field()
